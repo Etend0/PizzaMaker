d81c0f77c676bd986b42cfc3da11639a4437064a
Etendo <etendo123@gmail.com>
2026-03-22 16:07:40 -0600

    Added and completed the UI for a second form to show the current pizza order.

--- FrmOrderDetails/Form1.Designer.cs
+++ FrmOrderDetails/Form1.Designer.cs
@@ -28,12 +28,32 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            lblOrderDetails = new Label();
+            SuspendLayout();
+            // 
+            // lblOrderDetails
+            // 
+            lblOrderDetails.AutoSize = true;
+            lblOrderDetails.Location = new Point(50, 55);
+            lblOrderDetails.Name = "lblOrderDetails";
+            lblOrderDetails.Size = new Size(75, 15);
+            lblOrderDetails.TabIndex = 0;
+            lblOrderDetails.Text = "Order Details";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
-            Text = "Form1";
+            Controls.Add(lblOrderDetails);
+            Name = "Form1";
+            Text = "Pizza Order Details";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label lblOrderDetails;
     }
 }
