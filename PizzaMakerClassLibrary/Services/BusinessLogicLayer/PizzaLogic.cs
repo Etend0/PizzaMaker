bb4bf37a408547b6b7dbe47bc21195503e623dac
Etendo <etendo123@gmail.com>
2026-07-20 21:26:45 -0600

    Added a class library and pizza DAO and pizza logic classes.

---
+++ PizzaMakerClassLibrary/Services/BusinessLogicLayer/PizzaLogic.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * Elijah Hodge
+ * CST - 250
+ * 07/20/2026
+ * Pizza Maker
+ * Activity 4
+*/
+
+namespace PizzaMakerClassLibrary.Services.BusinessLogicLayer
+{
+    internal class PizzaLogic
+    {
+    }
+}
---
+++ PizzaMakerClassLibrary/Services/DataAccessLayer/PizzaDAO.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * Elijah Hodge
+ * CST - 250
+ * 07/20/2026
+ * Pizza Maker
+ * Activity 4
+*/
+
+namespace PizzaMakerClassLibrary.Services.DataAccessLayer
+{
+    internal class PizzaDAO
+    {
+        // Class level variables
+        private List<PizzaModel> _pizzaOrder;
+
+        /// <summary>
+        /// Default constructor for the pizza DAO
+        /// </summary>
+        public PizzaDAO()
+        {
+            _pizzaOrder = new List<PizzaModel>();
+        }
+    }
+}
